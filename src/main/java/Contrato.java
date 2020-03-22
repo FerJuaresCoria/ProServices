ff6523f11cb69b07b1cd2b5a13a1e4d653cbe727
FerJuaresCoria <fernandojuarescoria@gmail.com>
2020-03-22 18:59:01 -0300

    Primer commit

---
+++ src/main/java/Contrato.java
@@ -0,0 +1,11 @@
+public class Contrato {
+
+    private Prestador prestador;
+    private Consumidor consumidor;
+    private Tarea tarea;
+    private String fecha;
+    private int hora;
+    private String direccion;
+    private EstadoContrato estado;
+
+}
